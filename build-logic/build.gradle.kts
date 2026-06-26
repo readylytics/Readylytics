badda39318deb358dbca4fe7caf27c14ed899d41
Krenol <16196236+GregorLauritz@users.noreply.github.com>
2026-06-26 17:11:33 +0200

    build: align convention build logic deps

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,9 +2,15 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
 dependencies {
     compileOnly("com.android.tools.build:gradle:9.2.1")
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+    compileOnly("org.jetbrains.kotlin.android:org.jetbrains.kotlin.android.gradle.plugin:2.3.21")
     compileOnly("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.3.7")
     compileOnly("androidx.room:room-gradle-plugin:2.8.4")
 }
