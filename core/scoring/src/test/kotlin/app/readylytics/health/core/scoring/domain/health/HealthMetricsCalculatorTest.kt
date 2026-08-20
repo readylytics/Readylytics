d77e61aadd1f02f493f3fa24c4045ce1ee9336ab
Lauritz Gröger <16196236+GregorLauritz@users.noreply.github.com>
2026-08-20 11:12:08 +0200

    refactor: align core/scoring domain.health package with module namespace

--- core/scoring/src/test/kotlin/app/readylytics/health/core/scoring/domain/health/HealthMetricsCalculatorTest.kt
+++ core/scoring/src/test/kotlin/app/readylytics/health/core/scoring/domain/health/HealthMetricsCalculatorTest.kt
@@ -1,4 +1,4 @@
-package app.readylytics.health.domain.health
+package app.readylytics.health.core.scoring.domain.health
 
 import app.readylytics.health.core.model.data.preferences.Gender
 import app.readylytics.health.core.model.data.preferences.PhysiologyProfile
