338a394bff55b01091fa497e3b91c8092395be2f
Lauritz Gröger <16196236+GregorLauritz@users.noreply.github.com>
2026-09-03 12:17:17 +0200

    test(settings): add end-to-end search-to-highlighted-item flow test

---
+++ feature/settings/src/androidTest/kotlin/app/readylytics/health/feature/settings/SettingsSearchNavigationFlowTest.kt
@@ -0,0 +1,48 @@
+package app.readylytics.health.feature.settings
+
+import androidx.compose.material3.Surface
+import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SettingsSearchNavigationFlowTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun searchingForRasScaling_navigatesToTrainingScreen_andShowsTheControl() {
+        composeTestRule.setContent {
+            Surface {
+                SettingsScreen(
+                    thresholdState = ThresholdSettingsState(),
+                    sleepState = SleepSettingsState(),
+                    physiologyState = PhysiologySettingsState(),
+                    heartRateState = HeartRateZonesState(),
+                    localBackupState = LocalBackupState(),
+                    syncState = SyncSettingsState(),
+                    uiState = UIState(),
+                    dashboardCardsState = DashboardCardsSettingsState(),
+                    onThresholdEvent = {},
+                    onSleepEvent = {},
+                    onPhysiologyEvent = {},
+                    onHeartRateEvent = {},
+                    onLocalBackupEvent = {},
+                    onSyncEvent = {},
+                    onUIEvent = {},
+                    onDashboardCardsEvent = {},
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithText("Search settings…", substring = true).performTextInput("ras scaling")
+        composeTestRule.onNodeWithText("RAS Scaling Factor", substring = true).performClick()
+
+        composeTestRule.onNodeWithText("RAS Scaling Factor", substring = true).assertExists()
+    }
+}
